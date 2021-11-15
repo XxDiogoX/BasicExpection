7233b02bb0759f8d26ded711e8b872ad5c8098bc
XxDiogoX <diogorodriguesk02@gmail.com>
2021-11-15 16:46:15 -0300

    Projetinho básico de exceções

---
+++ ExercicioException/Entities/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExercicioException.Entities.Exceptions;
+using System.Globalization;
+
+namespace ExercicioException.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if(amount > WithDrawLimit && amount < Balance)
+            {
+                throw new DomainException("Withdrawn: The amount exceeds withdraw limit!!");
+            }
+
+            if(amount > Balance && amount < WithDrawLimit)
+            {
+                throw new DomainException("withdraw: Not enough balance!!");
+            }
+
+         
+
+            Balance -= amount;
+            WithDrawLimit -= amount;
+            Console.WriteLine("New Balance: " + Balance.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Number Account: {Number}\nHolder: {Holder}\nInitial balance: {Balance.ToString("F2", CultureInfo.InvariantCulture)}\nWithDraw Limit: {WithDrawLimit.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExercicioException/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using ExercicioException.Entities;
+using ExercicioException.Entities.Exceptions;
+
+namespace ExercicioException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                
+                
+
+                    Console.WriteLine("Enter account data:");
+                    Console.WriteLine("Number of account:");
+                    Console.Write("Number: ");
+                    int numberaccount = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Name of holder:");
+                    Console.Write("Name: ");
+                    string nameholder = Console.ReadLine();
+                    Console.WriteLine("Enter with value balance:");
+                    Console.Write("Value: ");
+                    double valueinitial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.WriteLine("Enter with value withdraw limit:");
+                    Console.Write("Value: ");
+                    double valuewithdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Account account = new Account(numberaccount, nameholder, valueinitial, valuewithdraw);
+                    Console.WriteLine();
+                    Console.WriteLine(account);
+                    Console.WriteLine();
+                    Console.WriteLine("Enter amount  for withdraw:");
+                    Console.Write("Value: ");
+                    double bavalue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    account.WithDraw(bavalue);
+               
+                   
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error " + e.Message);
+            }
+        }
+    }
+}
